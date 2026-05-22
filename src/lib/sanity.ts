65cf62837fbe83bcaa79d9638d435c006bfc113a
Laís Rodrigues <devlaisrodrigues@gmail.com>
2026-05-22 10:59:45 -0300

    fix: use named export createImageUrlBuilder to remove deprecation warning

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -1,5 +1,5 @@
 import { createClient } from '@sanity/client'
-import imageUrlBuilder from '@sanity/image-url'
+import { createImageUrlBuilder } from '@sanity/image-url'
 
 export const sanityClient = createClient({
   projectId: import.meta.env.VITE_SANITY_PROJECT_ID ?? '',
@@ -8,7 +8,7 @@ export const sanityClient = createClient({
   apiVersion: '2024-01-01',
 })
 
-const builder = imageUrlBuilder(sanityClient)
+const builder = createImageUrlBuilder(sanityClient)
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function urlFor(source: any) {
